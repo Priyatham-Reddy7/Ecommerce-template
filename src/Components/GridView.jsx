f88a40754c09a392e159a7aa1c03ea01caf6d88d
Priyatham Reddy <priyathamreddy147@gmail.com>
2022-03-24 21:23:27 +0530

    grid view optimized

--- src/Components/GridView.jsx
+++ src/Components/GridView.jsx
@@ -1,109 +1,36 @@
-import iphone13 from '../images/iphone13.jpg'
-import ipad from '../images/ipad.jpg'
-import applewatch from '../images/applewatch.jpg'
-import speaker from '../images/speaker.jpg'
+import productData from "./productData"
 
 const GridView = () => {
   return (
       <>
           <div className="card-group mb-4">
-              <div className="card">
-                  <img className="img-fluid h-100" src={ipad} alt="" />
-                  <div className="card-body">
-                        <h5 className="card-title text-center">
-                            Ipad Air mini
-                        </h5>
-                        <p className="card-text text-center">
-                            $499.99
-                        </p>
-                        <button type="button" className="btn btn-primary d-grid mx-auto">
-                            Add to Cart
-                        </button>
-                        <p className="card-title text-center mt-3 mb-0">
-                            <a className='nav-link active text-dark' href="#">
-                                Add to Wishlist
-                            </a>
-                        </p>
-                        <p className="card-title text-center">
-                            <a className='nav-link active text-dark'href="#">
-                                Add to compare
-                            </a>
-                        </p>
-                  </div>
+                {productData.map((product) => (
+                    <div className="card" key={product.id}>
+                        <img className="img-fluid h-100" src={product.img} alt="" />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">
+                                {product.title}
+                            </h5>
+                            <p className="card-text text-center">
+                                {product.price}
+                            </p>
+                            <button type="button" className="btn btn-primary d-grid mx-auto">
+                                Add to Cart
+                            </button>
+                            <p className="card-title text-center mt-3 mb-0">
+                                <a className='nav-link active text-dark' href="#">
+                                    Add to Wishlist
+                                </a>
+                            </p>
+                            <p className="card-title text-center">
+                                <a className='nav-link active text-dark'href="#">
+                                    Add to compare
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                ))}
               </div>
-              <div className="card">
-                  <img className="img-fluid h-100" src={iphone13} alt="" />
-                  <div className="card-body">
-                        <h5 className="card-title text-center">
-                            Iphone 13
-                        </h5>
-                        <p className="card-text text-center">
-                            $899.99
-                        </p>
-                        <button type="button" className="btn btn-primary d-grid mx-auto">
-                            Add to Cart
-                        </button>
-                        <p className="card-title text-center mt-3 mb-0">
-                            <a className='nav-link active text-dark' href="#">
-                                Add to Wishlist
-                            </a>
-                        </p>
-                        <p className="card-title text-center">
-                            <a className='nav-link active text-dark'href="#">
-                                Add to compare
-                            </a>
-                        </p>
-                  </div>
-              </div>
-              <div className="card">
-                  <img className="img-fluid h-100" src={applewatch} alt="" />
-                  <div className="card-body">
-                        <h5 className="card-title text-center">
-                            Apple Watch SE
-                        </h5>
-                        <p className="card-text text-center">
-                            $359.99
-                        </p>
-                        <button type="button" className="btn btn-primary d-grid mx-auto">
-                            Add to Cart
-                        </button>
-                        <p className="card-title text-center mt-3 mb-0">
-                            <a className='nav-link active text-dark' href="#">
-                                Add to Wishlist
-                            </a>
-                        </p>
-                        <p className="card-title text-center">
-                            <a className='nav-link active text-dark'href="#">
-                                Add to compare
-                            </a>
-                        </p>
-                  </div>
-              </div>
-              <div className="card">
-                  <img className="img-fluid h-100" src={speaker} alt="" />
-                  <div className="card-body">
-                        <h5 className="card-title text-center">
-                            Homepod mini
-                        </h5>
-                        <p className="card-text text-center">
-                            $299.99
-                        </p>
-                        <button type="button" className="btn btn-primary d-grid mx-auto">
-                            Add to Cart
-                        </button>
-                        <p className="card-title text-center mt-3 mb-0">
-                            <a className='nav-link active text-dark' href="#">
-                                Add to Wishlist
-                            </a>
-                        </p>
-                        <p className="card-title text-center">
-                            <a className='nav-link active text-dark'href="#">
-                                Add to compare
-                            </a>
-                        </p>
-                  </div>
-              </div>
-          </div>
 
           {/* pager */}
             <nav>
---
+++ src/Components/productData.js
@@ -0,0 +1,33 @@
+import iphone13 from '../images/iphone13.jpg'
+import ipad from '../images/ipad.jpg'
+import applewatch from '../images/applewatch.jpg'
+import speaker from '../images/speaker.jpg'
+
+const productData = [
+    {   
+        id: 1,
+        img: ipad,
+        title: 'Ipad Air',
+        price: '$499.99'
+    },
+    {   
+        id: 2,
+        img: iphone13,
+        title: 'Iphone13',
+        price: '$899.99'
+    },
+    {
+        id: 3,
+        img: applewatch,
+        title: 'Apple Watch',
+        price: '$359.99'
+    },
+    {
+        id: 4,
+        img: speaker,
+        title: 'Homepod Mini',
+        price: '$299.99'
+    }
+]
+
+export default productData
